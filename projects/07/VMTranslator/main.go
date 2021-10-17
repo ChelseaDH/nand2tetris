b7e25f6dbcf36424f7365292e12e1a34e7c7e7cb
Chelsea Hilditch <git@cdhildit.ch>
2021-10-17 13:41:17 +0100

    Project 8: add support for directories
    Adds supports for the use of a directory containing one or more .vm files as input, in preparation for handling function calls between files.
    
    The output file contains the combined translation of all .vm files within the directory, and has the name <directory_name>.asm.

--- projects/07/VMTranslator/main.go
+++ projects/07/VMTranslator/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"path"
@@ -13,29 +14,70 @@ import (
 
 func main() {
 	args := os.Args
-	if len(args) != 2  {
-		 log.Fatal("Incorrect number of command line arguments provided")
+	if len(args) != 2 {
+		log.Fatal("Incorrect number of command line arguments provided")
 	}
 
-	inputFilename := args[1]
-	inputFile, err := os.Open(inputFilename)
+	name := args[1]
+	fileInfo, err := os.Stat(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch mode := fileInfo.Mode(); {
+	case mode.IsRegular():
+		outputFile, err := os.OpenFile(strings.Replace(name, ".vm", ".asm", 1), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer outputFile.Close()
+
+		namespace := strings.Replace(path.Base(name), ".vm", "", 1)
+		t := translator.Translator{
+			Namespace: namespace,
+			Output:    outputFile,
+		}
+
+		translateFile(name, t)
+		t.Terminate()
+
+	case mode.IsDir():
+		outputFile, err := os.OpenFile(path.Join(name, fmt.Sprintf("%s.asm", path.Base(name))), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer outputFile.Close()
+
+		files, err := os.ReadDir(name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		t := translator.Translator{
+			Output: outputFile,
+		}
+
+		for _, file := range files {
+			if path.Ext(file.Name()) == ".vm" {
+				t.Namespace = strings.Replace(file.Name(), ".vm", "", 1)
+				translateFile(path.Join(name, file.Name()), t)
+			}
+		}
+		t.Terminate()
+
+	default:
+		log.Fatal("Second command line argument must be a .vm file or directory containing one or more .vm files")
+	}
+}
+
+func translateFile(path string, translator translator.Translator) {
+	inputFile, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer inputFile.Close()
-	inputFileRef := strings.Replace(path.Base(inputFilename), ".vm", "", 1)
-
-	outputFile, err := os.OpenFile(strings.Replace(inputFilename, ".vm", ".asm", 1), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer outputFile.Close()
 
 	scanner := bufio.NewScanner(inputFile)
-	t := translator.Translator{
-		Namespace: inputFileRef,
-		Output:    outputFile,
-	}
 	for scanner.Scan() {
 		command, err := parser.Parse(scanner.Text())
 		if err != nil {
@@ -46,11 +88,9 @@ func main() {
 			continue
 		}
 
-		err = t.Translate(command)
+		err = translator.Translate(command)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-
-	t.Terminate()
 }
